a27e1971cd9ec8ceef9011ea929e967b38cfac73
Mario Campana <mcampana555@hotmail.com>
2023-11-15 21:44:40 -0300

    Primera entrega Proyecto ml

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+})
