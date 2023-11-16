3c0c757c323ad6dcb33ffe5c72d00f4ffca4f709
Mario Campana <mcampana555@hotmail.com>
2023-11-15 21:52:54 -0300

    Correccion de app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 })
